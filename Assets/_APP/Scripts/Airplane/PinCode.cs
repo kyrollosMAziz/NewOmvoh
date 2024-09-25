3a7dd05a83fb8f0ce2f7d00b7d6d74298d9a9be7
Kyrollos Aziz <80505582+kyrollosMAziz@users.noreply.github.com>
2024-09-25 14:04:36 +0300

    PinCode Logic Completed

--- Assets/_APP/Scripts/Airplane/PinCode.cs
+++ Assets/_APP/Scripts/Airplane/PinCode.cs
@@ -6,5 +6,20 @@ using UnityEngine;
 public class PinCode : ProjectContextSingleton<PinCode>
 {
     public TextMeshProUGUI m_Screen;
+    public List<IFailedPinCode> m_Pins = new List<IFailedPinCode>();
 
+    public void AssignPins(IFailedPinCode Pin) 
+    {
+        m_Pins.Add(Pin);
+        Debug.Log(Pin);
+    }
+    public void RemovePins() 
+    {
+        m_Pins = null;
+    }
+    public void NotifyPins() 
+    {
+        m_Pins.ForEach(p => p.OnPinCodeFail());
+        RemovePins();
+    }
 }
