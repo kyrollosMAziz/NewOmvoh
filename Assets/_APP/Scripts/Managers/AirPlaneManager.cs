7decbb276205cde3a6e20204494db72a27446776
Andrew Gamal <andrew@gamein.ae>
2024-10-02 19:07:56 +0300

    update airplane audios

--- Assets/_APP/Scripts/Managers/AirPlaneManager.cs
+++ Assets/_APP/Scripts/Managers/AirPlaneManager.cs
@@ -3,11 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 public class AirPlaneManager : MonoBehaviour
 {
     [SerializeField] private GameData _gameData;
     [SerializeField] private GameObject _playerXr;
+   
+    [SerializeField] private Volume _vignetteEffect;
 
     [SerializeField] private AudioSource _backGroundEffect;
     [SerializeField] private AudioSource _rollingAudio;
@@ -17,6 +20,10 @@ public class AirPlaneManager : MonoBehaviour
     [SerializeField] private AudioSource _voiceOverNPC1AudioSource;
     [SerializeField] private AudioSource _voiceOverNPC2AudioSource;
     [SerializeField] private AudioSource _turbulanceAudioSource;
+
+    [SerializeField] private AudioSource _calm;
+    [SerializeField] private AudioSource _taka;
+    [SerializeField] private AudioSource _shit;
 
     [SerializeField] private Transform _bathroomTransitionPosition;
     [SerializeField] private Transform _seatTransitionPosition;
@@ -158,18 +165,26 @@ public class AirPlaneManager : MonoBehaviour
         yield return new WaitForSeconds(_voiceOverAudioSource.clip.length + 1f);
         _voiceOverAudioSource.clip = _gameData.playerGender == GenderEnum.Male
              ? _maleBathroomClip2
-    :   _femaleBathroomClip2;
-         _voiceOverAudioSource.Play();
+    : _femaleBathroomClip2;
+        _voiceOverAudioSource.Play();
+
         yield return new WaitForSeconds(_voiceOverAudioSource.clip.length + 1f);
         _voiceOverAudioSource.clip = _gameData.playerGender == GenderEnum.Male
-             ? _maleBathroomClip3
-    :   _femaleBathroomClip3;
-         _voiceOverAudioSource.Play();
+             ? _maleBathroomClip3 : _femaleBathroomClip3;
+        _voiceOverAudioSource.Play();
+       
         yield return new WaitForSeconds(_voiceOverAudioSource.clip.length + 1f);
+        
+        StartCoroutine(StartEffect());
+        _taka.gameObject.SetActive(true);
+
         _voiceOverAudioSource.clip = _gameData.playerGender == GenderEnum.Male
-             ? _maleBathroomClip4
-    :   _femaleBathroomClip4;
-         _voiceOverAudioSource.Play();
+             ? _maleBathroomClip4 : _femaleBathroomClip4;
+        _voiceOverAudioSource.Play();
+
+        _calm.gameObject.SetActive(false);
+        _shit.gameObject.SetActive(true);
+
         yield return new WaitForSeconds(_voiceOverAudioSource.clip.length + 1f);
         VignetteFadeController.Instance.FadeImageOutWithAction(() =>
         {
@@ -179,6 +194,16 @@ public class AirPlaneManager : MonoBehaviour
             AudioSourcesAction(false);
         });
         //open door action
+    }
+
+    private IEnumerator StartEffect()
+    {
+        _vignetteEffect.weight = 0;
+        while (_vignetteEffect.weight < 0.8)
+        {
+            _vignetteEffect.weight += Time.deltaTime / 1;
+            yield return null;
+        }
     }
 
     public IEnumerator OnBathroomKeypadInteraction()
@@ -215,7 +240,7 @@ public class AirPlaneManager : MonoBehaviour
         yield return new WaitForSeconds(7f);
         VignetteFadeController.Instance.FadeImageInWithAction(() =>
         {
-          //  _backGroundEffect.clip = _chatterAudio;
+            //  _backGroundEffect.clip = _chatterAudio;
             _rollingAudio.gameObject.SetActive(false);
             AudioSourcesAction(true);
             StartCoroutine(ExposureBehavior());
@@ -254,7 +279,7 @@ public class AirPlaneManager : MonoBehaviour
         yield return new WaitForSeconds(_voiceOverAudioSource.clip.length + 1f);
         VignetteFadeController.Instance.FadeImageOutWithAction(() =>
         {
-              LoadLobby(); 
+            LoadLobby();
         });
     }
 
